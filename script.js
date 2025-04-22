42aa74169b7fc3be56be5b76e6aff7c2074272a8
gabrielaagata <145785076+gabrielaagata@users.noreply.github.com>
2025-04-22 08:15:36 -0300

    22/04/25

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    const alternaContraste= document.getElementById('alterna-contraste');
+
+
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function () {
@@ -22,6 +25,10 @@ document.addEventListener('DOMContentLoaded', function () {
     diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+        alternaContraste.addEventListener('click', function(){
+            document.body.classList.toggle('alto-contraste');
+        })
     });
 
 });
