b8b8338a44ef2a748d53cc093441284f72111c32
gabrielaagata <145785076+gabrielaagata@users.noreply.github.com>
2025-04-22 08:20:49 -0300

    22/04

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
         alternaContraste.addEventListener('click', function () {
             document.body.classList.toggle('alto-contraste');
         })
-
     });
+
+});
 
